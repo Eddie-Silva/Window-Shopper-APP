7deb59a6df781a0ed422f7dbe9a898882c53274e
admin <admin@admins-MacBook-Pro.local>
2018-06-21 15:20:56 -0500

    the start

---
+++ WindowShopper/WindowShopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WindowShopper
+//
+//  Created by admin on 6/21/18.
+//  Copyright © 2018 Burgeoning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
